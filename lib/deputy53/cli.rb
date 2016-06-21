2057018200000920d1617a6491ac95e3efa32e6c
Chris Olstrom <chris@olstrom.com>
2016-06-21 16:44:39 -0700

    Add assign command to CLI

--- lib/deputy53/cli.rb
+++ lib/deputy53/cli.rb
@@ -1,9 +1,17 @@
 require_relative 'agent'
+require_relative 'assigner'
 
 module Deputy53
+  # CommandLine Interface
   class CLI
     def delegate(subdomain)
       Agent.new(subdomain).delegate
+    end
+
+    def assign(subdomain, user = nil)
+      subdomain = "#{subdomain}." unless subdomain.end_with? '.'
+      user ||= subdomain.split('.').slice(0..-3).join('.')
+      Assigner.new.assign(subdomain, user)
     end
   end
 end
